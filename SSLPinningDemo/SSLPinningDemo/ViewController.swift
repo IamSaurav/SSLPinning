2aa86b9c623af4772baf9c4f2b62d34af503dd48
Saurav Satpathy <saurav.satpathy@genpact.digital>
2020-04-30 21:29:12 +0530

    updated

--- SSLPinningDemo/SSLPinningDemo/ViewController.swift
+++ SSLPinningDemo/SSLPinningDemo/ViewController.swift
@@ -25,7 +25,6 @@ class ViewController: UIViewController, URLSessionDelegate {
         super.viewDidLoad()
         sendRequest()
     }
-    
     func sendRequest() {
         let url = URL(string: "https://www.bitmountn.com/")!
         let session = URLSession( configuration: URLSessionConfiguration.ephemeral, delegate: self, delegateQueue: .none)
